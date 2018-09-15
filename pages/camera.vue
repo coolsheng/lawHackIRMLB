0c1cec8cd6fe9e32c9ded2acedc41098ead6b777
Sheng <shengzengg@gmail.com>
2018-09-15 10:42:44 +1000

    back phone camera

--- pages/camera.vue
+++ pages/camera.vue
@@ -28,11 +28,10 @@
                 captures: []
             }
         },
-        // {facingMode: {exact: "environment"}}
         mounted() { 
     this.video = this.$refs.video;
     if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        navigator.mediaDevices.getUserMedia({ video:  {facingMode: {exact: "environment"}} }).then(stream => {
             this.video.src = window.URL.createObjectURL(stream);
             this.video.play();
         });
